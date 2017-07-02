290e6177faab38058160a10391c3d085b71da42e
Kohei Watanabe <humilitiny@gmail.com>
2017-07-02 19:38:16 +0900

    first commit

---
+++ feature_extractor.py
@@ -0,0 +1,82 @@
+import os, time, json, glob
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+# from matplotlib import pyplot as plt 
+# %matplotlib inline
+
+import pylab
+import imageio
+import cv2
+
+from keras.models import Model
+from inception_v3 import InceptionV3, preprocess_input
+from keras.preprocessing import image
+from imagenet_utils import decode_predictions
+
+from util import mkdir_if_not_exist
+
+
+def get_img_list_from_vid_reader(vid, extension, reshape_size=(299, 299), MAX_N_FRAME=300):
+    num = 0
+    fps = int(round(vid.get_meta_data()['fps']))
+    img_list = []
+
+    flg = False
+    for i in range(MAX_N_FRAME):
+        num = i * fps
+        try:
+            img = vid.get_data(num)
+        except:
+            return  np.array(img_list)
+        
+        if extension == '3gp':
+            img = img[25:120,:]  # cropping
+            if flg:
+                cv2.imwrite('a.png',img)
+                flg = False
+            
+        
+        img = cv2.resize(img, reshape_size).astype(np.float32)
+        img_list.append(img)
+                         
+    return  np.array(img_list)
+
+def get_feature_mat(mymodel, input_data, batch_size=128):
+    idx = 0
+    preds_mat = None
+    while(idx < len(input_data)):
+        x = input_data[idx:idx+batch_size]
+        x = preprocess_input(x)
+        preds = mymodel.predict(x)
+
+        if preds_mat is None:
+            preds_mat = preds
+        else:
+            preds_mat = np.concatenate([preds_mat, preds])
+        idx += batch_size
+        
+    return preds_mat.astype(np.float32)
+
+def get_feature_mat_from_video(video_filename, output_dir='output'):
+    yt_vid, extension = video_filename.split('/')[-1].split('.')
+    
+    assert extension in ['webm', 'mp4', '3gp']
+
+    mkdir_if_not_exist(output_dir, False)
+    
+    output_filename = output_dir + '/' + yt_vid +'.npy'
+
+    vid_reader = imageio.get_reader(video_filename,  'ffmpeg')
+
+    img_list = get_img_list_from_vid_reader(vid_reader, extension)
+    
+    base_model = InceptionV3(include_top=True, weights='imagenet')
+    model = Model(inputs=base_model.input, outputs=base_model.get_layer('avg_pool').output)
+    
+    feature_mat = get_feature_mat(model, img_list)
+    
+    np.save(output_filename, feature_mat)
+
+    return feature_mat
---
+++ video_label_prediction.py
@@ -0,0 +1,65 @@
+from feature_extractor import get_feature_mat_from_video
+from my_pca import get_pcaed_feature
+
+from tqdm import tqdm_notebook as tqdm
+import tensorflow as tf
+import numpy as np
+
+from yt8m_utils import Dequantize
+import pprint
+
+import pandas as pd
+
+
+def print_predicted_label(feature, topn=10,  latest_checkpoint = './yt8m_model/model.ckpt-2833', id2label_csv='./label_names.csv'):
+
+    id2label_ser = pd.read_csv(id2label_csv, index_col=0)
+    id2label = id2label_ser.to_dict()['label_name']
+
+    meta_graph_location = latest_checkpoint + ".meta"
+
+    sess = tf.InteractiveSession()
+
+    saver = tf.train.import_meta_graph(meta_graph_location, clear_devices=True)
+    saver.restore(sess, latest_checkpoint)
+
+    input_tensor = tf.get_collection("input_batch_raw")[0]
+    num_frames_tensor = tf.get_collection("num_frames")[0]
+    predictions_tensor = tf.get_collection("predictions")[0]
+
+    # Workaround for num_epochs issue.
+    def set_up_init_ops(variables):
+        init_op_list = []
+        for variable in list(variables):
+            if "train_input" in variable.name:
+                init_op_list.append(tf.assign(variable, 1))
+                variables.remove(variable)
+        init_op_list.append(tf.variables_initializer(variables))
+        return init_op_list
+
+    sess.run(set_up_init_ops(tf.get_collection_ref(
+            tf.GraphKeys.LOCAL_VARIABLES)))
+    
+    
+
+    padded_feature = np.zeros([300, 1024])
+    padded_feature[:feature.shape[0], :] = Dequantize(feature)
+    video_batch_val = padded_feature[np.newaxis, :, :].astype(np.float32)
+    num_frames_batch_val = np.array([feature.shape[0]], dtype=np.int32)
+
+    predictions_val, = sess.run([predictions_tensor], feed_dict={input_tensor: video_batch_val,
+                                                                         num_frames_tensor: num_frames_batch_val})
+
+    predictions_val = predictions_val.flatten()
+
+    top_idxes = np.argsort(predictions_val)[::-1][:topn]
+
+    pprint.pprint ([(id2label[x], predictions_val[x]) for x in top_idxes])
+
+
+
+feature_2048_mat = get_feature_mat_from_video('gakky.webm')
+
+pca_feature = get_pcaed_feature(feature_2048_mat)
+
+print_predicted_label(pca_feature)
